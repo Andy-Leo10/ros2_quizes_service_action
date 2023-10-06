904e2444e1c82b28020beef6d5982f285449bca5
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2023-10-06 11:38:35 -0500

    update for service server

--- services_quiz/src/services_quiz_server.cpp
+++ services_quiz/src/services_quiz_server.cpp
@@ -8,9 +8,11 @@ using Spin = services_quiz_srv::srv::Spin;
 using std::placeholders::_1;
 using std::placeholders::_2;
 
-class ServerNode : public rclcpp::Node {
+class ServerNode : public rclcpp::Node
+{
 public:
-  ServerNode() : Node("service_rotating") {
+  ServerNode() : Node("service_rotating")
+  {
 
     srv_ = create_service<Spin>(
         "rotate", std::bind(&ServerNode::moving_callback, this, _1, _2));
@@ -26,36 +28,49 @@ private:
   int time_ = 0;
   bool success_ = false;
   void moving_callback(const std::shared_ptr<Spin::Request> request,
-                       const std::shared_ptr<Spin::Response> response) {
+                       const std::shared_ptr<Spin::Response> response)
+  {
 
     auto message = geometry_msgs::msg::Twist();
     direction_ = request->direction;
     angular_velocity_ = request->angular_velocity;
     time_ = request->time;
+    rclcpp::Rate loop_rate(1);
 
-    if (direction_ == "right") {
-      message.linear.x = 0.0;
-      message.angular.z = -1 * angular_velocity_;
-      publisher_->publish(message);
-      success_ = true;
-    } else if (direction_ == "left") {
-      message.linear.x = 0.0;
-      message.angular.z = 1 * angular_velocity_;
-      publisher_->publish(message);
-      success_ = true;
-    } else {
-      message.linear.x = 0.0;
-      message.angular.z = 0;
-      publisher_->publish(message);
-      success_ = false;
+    for (int i = 0; rclcpp::ok(i < time_) && (); i++)
+    {
+      if (direction_ == "right")
+      {
+        message.linear.x = 0.0;
+        message.angular.z = -1 * angular_velocity_;
+        publisher_->publish(message);
+        success_ = true;
+      }
+      else if (direction_ == "left")
+      {
+        message.linear.x = 0.0;
+        message.angular.z = 1 * angular_velocity_;
+        publisher_->publish(message);
+        success_ = true;
+      }
+      else
+      {
+        message.linear.x = 0.0;
+        message.angular.z = 0;
+        publisher_->publish(message);
+        success_ = false;
+        break;
+      }
+      loop_rate.sleep();
     }
     response->success = success_;
   }
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   rclcpp::init(argc, argv);
-  auto server_node = std::make_shared<ServerNode>();  // Crear una instancia de ServerNode
+  auto server_node = std::make_shared<ServerNode>(); // Crear una instancia de ServerNode
   rcutils_logging_set_logger_level(server_node->get_logger().get_name(), RCUTILS_LOG_SEVERITY_DEBUG);
   RCLCPP_DEBUG(server_node->get_logger(), "SERVICE = /rotate");
   RCLCPP_INFO(server_node->get_logger(), "FOR TEST USE = ros2 service call /rotate services_quiz_srv/srv/Spin \"{direction: 'right', angular_velocity: 1.0, time: 5}\"");
